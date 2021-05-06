9dbff7e8f753df2ea5799bec5f3bcf1ef16f431e
Federico Nahuel Gonzalez <nahuelfedericog@gmail.com>
2021-05-06 10:01:31 -0300

    It finds the min and max

--- pandas_exerc.py
+++ pandas_exerc.py
@@ -3,17 +3,24 @@ import openpyxl as op
 
 workbook= op.load_workbook("C:/Users/Rodolfo/Documents/FNG/Autodidactismo/Finanzas/DB/DB.xlsx")
 excel_sheet= workbook.active
-
 ###Obtengo títulos
 titles=next(excel_sheet.values)[0:]
 
 ###Creo el dataframe
-qcom_dataframe= pd.DataFrame(excel_sheet.values, columns=titles)
+dirty_qcom_dataframe= pd.DataFrame(excel_sheet.values, columns=titles)
 
 ###Agarro los datos que me interesan
 relevant_columns=['Date', 'Close/Last', 'Variation%', 'Volume', 'Open', 'High', 'Low']
-qcom_fixed= qcom_dataframe[relevant_columns]
+qcom_dataframe= dirty_qcom_dataframe[relevant_columns]
 
 ###Ya puedo borrar la primera fila porque ya tengo los títulos
-qcom_fixed=qcom_fixed.drop(qcom_fixed.index[0])
-print(qcom_fixed.describe())
+qcom_dataframe=qcom_dataframe.drop(qcom_dataframe.index[0])
+
+#print(qcom_dataframe.describe())
+qcom_close_prices = qcom_dataframe[['Close/Last']]
+
+#print(qcom_close_prices)
+
+max_value_stock= qcom_dataframe['High'].min(axis=0)
+min_value_stock=qcom_dataframe['Low'].min(axis=0)
+
