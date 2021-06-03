e532409c68f7723bb1fb0d7f9c5c05b224207a76
Federico Nahuel Gonzalez <nahuelfedericog@gmail.com>
2021-06-03 10:57:28 -0300

    comment what do each part of the new function

--- backtesting/emas_7_21.py
+++ backtesting/emas_7_21.py
@@ -33,14 +33,20 @@ def create_mean_in_df(mean, df):
     df[f"mean_{mean}"] = calcular_media(df, mean)
     return f"mean_{mean}"
 
-
+#quiero que me genere una nueva columna en la que me diga cuando estar largo o corto
 def buy_or_sell(list_of_mean, df):
-
+    
     for day in range(len(df)):
+        
+        #si la mediana no tiene valores, entonces que no calcule nada
         if df.loc[day, list_of_mean[0]] == 0.00 or df.loc[day, list_of_mean[1]] == 0.00:
             pass
-        elif df.loc[day, list_of_mean[0]] > df.loc[day, list_of_mean[1]]:
+        
+        #si la mediana mas chica tiene un valor mas alto que la mas grande o igual, entonces que compre
+        elif df.loc[day, list_of_mean[0]] >= df.loc[day, list_of_mean[1]]:
             df.loc[day, "status"] = "long"
+        
+        #en cualquier otro caso, sera que la mediana mas chica es mas pequenna
         else:
             df.loc[day, "status"] = "short"
 
@@ -49,7 +55,6 @@ def run():
     list_of_mean.append(create_mean_in_df(10, spy_df))
     list_of_mean.append(create_mean_in_df(50, spy_df))
     buy_or_sell(list_of_mean, spy_df)
-    print(spy_df)
 
 
 if __name__ == "__main__":
